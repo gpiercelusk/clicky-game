647b7c81d450c53d8d8298df2a574a6c2526bfee
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-06-25 22:22:20 -0400

    added files for what i want to do

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import CharactersCard from './components/characterCard'
+import Footer from './components/footer'
+import characters from "./character.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+//sets state to 0 
+class App extends Component {
+  state = {
+    characters,
+    clickedCharacters: [],
+    score: 0
+  }
+
+  //the order of components to be rendered: navbar, Hero, CharacterCard, footer 
+  render() {
+    return (
+      <>
+        <p></p>
+        <Navbar score={this.state.score} />
+        <Hero />
+        <section className="section is-warning wrapper">
+
+          {this.state.characters.map(character => (
+            <CharactersCard
+              imageClick={this.imageClick}
+              id={character.id}
+              key={character.id}
+              image={character.image}
+            />
+          ))}
+
+        </section>
+        <Footer />
+      </>
+    );
+  }
+}
 export default App;
---
+++ src/components/characterCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '/CharacterCard.css';
+
+function CharacterCard(props) {
+  return (
+    <img class='card' alt={props.id} src={props.image} onClick={props.imageClick} />
+  )
+}
+
+export default CharacterCard
